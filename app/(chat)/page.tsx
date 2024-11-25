52541c804ed34ae49b9f7be41e5567f03e5526a9
williamhliu <williamhliu@tencent.com>
2024-11-25 08:34:27 +0800

    test deploy

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,18 +1,20 @@
-import { cookies } from 'next/headers';
+import { cookies } from "next/headers";
 
-import { Chat } from '@/components/chat';
-import { DEFAULT_MODEL_NAME, models } from '@/lib/ai/models';
-import { generateUUID } from '@/lib/utils';
+import { Chat } from "@/components/chat";
+import { DEFAULT_MODEL_NAME, models } from "@/lib/ai/models";
+import { generateUUID } from "@/lib/utils";
 
 export default async function Page() {
   const id = generateUUID();
 
   const cookieStore = await cookies();
-  const modelIdFromCookie = cookieStore.get('model-id')?.value;
+  const modelIdFromCookie = cookieStore.get("model-id")?.value;
 
   const selectedModelId =
     models.find((model) => model.id === modelIdFromCookie)?.id ||
     DEFAULT_MODEL_NAME;
+
+  console.log("test");
 
   return (
     <Chat
